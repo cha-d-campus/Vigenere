ff334f36cd410f3c4c510e7a6f22f048e3e70dd4
Cha <cd.webdesign.graphism@gmail.com>
2022-05-10 13:52:34 +0200

    update

---
+++ src/Crypto.java
@@ -0,0 +1,43 @@
+public class Crypto {
+
+    private String alphabet;
+
+
+
+    public Crypto() {
+        this.alphabet = "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    public void encodage(String mot, String mdp){
+        String msg_chiffre = "";
+        for (int i = 0; i < mot.length(); i++) {
+            int index_modulo = i % mdp.length();
+            char matching_letter = mdp.charAt(index_modulo);
+            int decalage = alphabet.indexOf(matching_letter);
+            int lettre_encodee = alphabet.indexOf(mot.charAt(i)) + decalage;
+            char lettre_crypte = alphabet.charAt(lettre_encodee);
+
+//            System.out.println("i : " + i);
+//            System.out.println("lettre à encoder : " + mot.charAt(i));
+//            System.out.println("lettre encodée + decalage : " + lettre_encodee);
+//            System.out.println("nouvelle lettre = " + lettre_crypte);
+//            System.out.println(" % : " + index_modulo);
+//            System.out.println("lettre correspondante dans mdp : " + matching_letter);
+//            System.out.println("décalage : " + decalage);
+//            System.out.println();
+//            System.out.println("-------------------------------");
+//            System.out.println(lettre_crypte);
+//            System.out.println("-------------------------------");
+            msg_chiffre += lettre_crypte;
+        }
+        System.out.println("Le mot à chiffrer est : " + mot);
+        System.out.println("La clé de chiffrement est : " + mdp);
+        System.out.println("Le mot chiffré est : " +msg_chiffre);
+    }
+
+    public String getAlphabet(){
+        return this.alphabet;
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Crypto encoder = new Crypto();
+        String mdp = "deg";
+        String message = "toto";
+
+        System.out.println("\n---------- VERIFICATION DE NOTRE FONCTION -----------");
+        System.out.println("Mot à chiffrer : " + message);
+        System.out.println("Clé de chiffrement : " + mdp);
+        System.out.println("\n--------------------- INDEX MOT ----------------------");
+        System.out.println("Index de la lettre \"T\" dans l'alphabet : " + encoder.getAlphabet().indexOf("t"));
+        System.out.println("Index de la lettre \"O\" dans l'alphabet : " + encoder.getAlphabet().indexOf("o"));
+        System.out.println("--------------------- INDEX MDP ----------------------");
+        System.out.println("Index de la lettre \"D\" dans l'alphabet : " + encoder.getAlphabet().indexOf("d"));
+        System.out.println("Index de la lettre \"E\" dans l'alphabet : " + encoder.getAlphabet().indexOf("e"));
+        System.out.println("Index de la lettre \"G\" dans l'alphabet : " + encoder.getAlphabet().indexOf("g"));
+        System.out.println("----------------- INDEX MOT ENCODE ------------------");
+        int calc1 = encoder.getAlphabet().indexOf("t") + encoder.getAlphabet().indexOf("d");
+        int calc2 = encoder.getAlphabet().indexOf("o") + encoder.getAlphabet().indexOf("e");
+        int calc3 = encoder.getAlphabet().indexOf("t") + encoder.getAlphabet().indexOf("g");
+        int calc4 = encoder.getAlphabet().indexOf("o") + encoder.getAlphabet().indexOf("d");
+        System.out.println(encoder.getAlphabet().indexOf("t") + " + " + encoder.getAlphabet().indexOf("d") + " = " + calc1);
+        System.out.println(encoder.getAlphabet().indexOf("o") + " + " + encoder.getAlphabet().indexOf("e") + " = " + calc2);
+        System.out.println(encoder.getAlphabet().indexOf("t") + " + " + encoder.getAlphabet().indexOf("g") + " = " + calc3);
+        System.out.println(encoder.getAlphabet().indexOf("o") + " + " + encoder.getAlphabet().indexOf("d") + " = " + calc4);
+        System.out.println("-> Mot chiffré : " + encoder.getAlphabet().charAt(22) + encoder.getAlphabet().charAt(18) + encoder.getAlphabet().charAt(25) + encoder.getAlphabet().charAt(17));
+
+        System.out.println("\n--------------- APPEL DE NOTRE FONCTION --------------");
+        encoder.encodage(message, mdp);
     }
+
+
 }
